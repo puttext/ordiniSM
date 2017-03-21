85b7af0e0d6621640960e98d46fd9026b56582e2
Stefano Radaelli <s.radaelli@itsgenius.eu>
2017-03-21 20:05:46 +0100

    fix

--- app/Http/Controllers/OrdiniController.php
+++ app/Http/Controllers/OrdiniController.php
@@ -40,7 +40,7 @@ class OrdiniController extends Controller
     		if (\Auth::user()->livello <= User::COORDINATORE){
     			$qGruppi->where(function($q){
 					$q->whereIn("id", Prodotto::whereFornitoreId(\Auth::user()->fornai->pluck("id")));
-					$q->orWhereIn("id",Prodotto::whereNot("tipo","=","pane")->get()->pluck("id"));
+					$q->orWhereIn("id",Prodotto::where("tipo","<>","pane")->get()->pluck("id"));
     			});
     		}
     	}
