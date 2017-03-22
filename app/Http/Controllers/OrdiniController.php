aabbd97454d170403813c88d0a99011eec52ebb4
Stefano Radaelli <s.radaelli@itsgenius.eu>
2017-03-22 04:14:34 +0100

    fix

--- app/Http/Controllers/OrdiniController.php
+++ app/Http/Controllers/OrdiniController.php
@@ -61,6 +61,9 @@ class OrdiniController extends Controller
     		
     		$gruppo["consegne"]=implode($consegne,", ");
     		
+    		$gruppo["url_edit"]="";
+    		$gruppo["url_compila"]="";
+    		
     		if (\Auth::user()->livello>=User::COORDINATORE){
 	    		if (substr($gruppo["codice_gruppo"],0,1)=="P")
 	    			$gruppo["url_edit"]=url('/ordini/pane/'.$ordini[0]->consegna->format("Y/m").'/edit/'.substr($gruppo["codice_gruppo"],2,1));
@@ -68,7 +71,6 @@ class OrdiniController extends Controller
 	    			$gruppo["url_edit"]=url('/ordini/'.$gruppo['codice_gruppo'].'/edit');
     		}
     		$gruppo["url_view"]=url('/ordini/'.$gruppo['codice_gruppo']);
-    		$gruppo["url_compila"]="";
     		
     		if ($ordini[0]->apertura>$oggi){
     			$gruppo["url_compila"]=url('/ordini/compila/'.$gruppo['codice_gruppo']);
