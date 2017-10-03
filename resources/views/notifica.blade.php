db36a1c815ca79acb3248e31e24e7207855596a0
Stefano Radaelli <s.radaelli@itsgenius.eu>
2017-10-03 10:01:21 +0200

    correzione email

--- resources/views/notifica.blade.php
+++ resources/views/notifica.blade.php
@@ -1,5 +1,5 @@
 Caro referente,
-e' stato aperto o modificato un ordine nella piattaforma ordiniSPiga e Madia.
+e' stato aperto o modificato un ordine nella piattaforma ordini Spiga e Madia.
 Per compilarlo accedi al seguente indirizzo:
 {{ $url }}
 
